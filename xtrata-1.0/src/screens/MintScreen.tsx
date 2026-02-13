7512c16ead618f75563233694654055de50c5c4a
stxtrata <stxtrata@gmail.com>
2026-02-13 22:29:21 +0000

    Summarised Docs

--- xtrata-1.0/src/screens/MintScreen.tsx
+++ xtrata-1.0/src/screens/MintScreen.tsx
@@ -2362,8 +2362,7 @@ export default function MintScreen(props: MintScreenProps) {
           </div>
           <p>Upload a file, review fees, and inscribe on-chain.</p>
           <p className="meta-value">
-            Public minting is available while the contract is unpaused. Partner
-            collection mints require allowlisting by the contract owner.
+            Public minting is open. Partner contracts coming soon.
           </p>
         </div>
         <div className="panel__actions">
